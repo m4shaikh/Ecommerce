9b352d68138c0b43b1575f8336d298623be8f497
m4shaikh <moin.shaikh.3656@gmail.com>
2025-08-09 21:50:57 +0530

    Changed Landing Page

--- ecom/ecom/urls.py
+++ ecom/ecom/urls.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views.generic.base import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='/accounts/login/', permanent=False)),
     path('accounts/', include('accounts.urls')),
     path('shop/',include('shop.urls', namespace = 'shop')),
     path('cart/',include('cart.urls', namespace = 'cart')),
